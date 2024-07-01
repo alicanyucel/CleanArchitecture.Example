0551971e67e694e0587e32cb75ea008192128cd4
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-01 21:56:49 +0300

    getall

---
+++ CleanArchitecture.Application/Features/Todos/GetAll/GetAllTodoQuery.cs
@@ -0,0 +1,7 @@
+﻿
+
+using CleanArchitecture.Domain.Entities;
+using MediatR;
+
+namespace CleanArchitecture.Application.Features.Todos.GetAll;
+public sealed record GetAllTodoQuery() : IRequest<List<Todo>>;
---
+++ CleanArchitecture.Application/Features/Todos/GetAll/GetAllTodoQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿
+
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
+using MediatR;
+
+namespace CleanArchitecture.Application.Features.Todos.GetAll;
+
+internal sealed class GetAllTodoQueryHandler(
+    ITodoRepository todoRepository) : IRequestHandler<GetAllTodoQuery, List<Todo>>
+{
+    public async Task<List<Todo>>Handle(GetAllTodoQuery request, CancellationToken cancellationToken)
+    {
+        List<Todo> todos = await todoRepository.GetAllAsync(cancellationToken);
+
+        return todos;
+    }
+}
