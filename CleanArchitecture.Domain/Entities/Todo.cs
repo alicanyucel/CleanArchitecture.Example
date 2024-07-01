0cc473e09b3c7eebc7220cf5400863bafa12ffec
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-01 20:43:07 +0300

    Add project files.

---
+++ CleanArchitecture.Domain/Entities/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities;
+public sealed class Todo
+{
+    public Guid Id { get; set; } //0000-000-000
+    public string Work { get; set; } = default!;
+    public DateOnly DeadLine { get; set; } //01-01-0001
+    public bool IsCompleted { get; set; } //false
+    public string? Note { get; set; }
+}
---
+++ CleanArchitecture.Domain/Repositories/ITodoRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Repositories;
+
+public interface ITodoRepository
+{
+    Task AddAsync(Todo item, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Todo item, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Todo item, CancellationToken cancellationToken = default);
+    Task<List<Todo>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Todo?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
