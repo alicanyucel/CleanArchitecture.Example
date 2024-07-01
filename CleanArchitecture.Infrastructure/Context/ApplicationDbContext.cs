83024a3f8a958e4b137cf3ac0917e5357a9737df
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-01 22:32:08 +0300

    repositories implement

---
+++ CleanArchitecture.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Context;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Todo> Todos { get; set; }
+}
+
---
+++ CleanArchitecture.Infrastructure/Repositories/TodoRepository.cs
@@ -0,0 +1,40 @@
+﻿
+
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
+using CleanArchitecture.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Repositories;
+
+internal sealed class TodoRepository(ApplicationDbContext context) : ITodoRepository
+{
+    public async Task AddAsync(Todo item, CancellationToken cancellationToken = default)
+    {
+        await context.AddAsync(item, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+    }
+
+    public async Task DeleteAsync(Todo item, CancellationToken cancellationToken = default)
+    {
+        context.Remove(item);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<List<Todo>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await context.Todos.ToListAsync(cancellationToken);
+    }
+
+    public async Task<Todo?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await context.Todos.FindAsync(id, cancellationToken);
+    }
+
+    public async Task UpdateAsync(Todo item, CancellationToken cancellationToken = default)
+    {
+        context.Update(item);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
