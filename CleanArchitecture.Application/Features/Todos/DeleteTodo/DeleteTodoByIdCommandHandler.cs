6d8903daaebaf30c7e794df4a3a84b9d96fa9af3
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-01 21:37:21 +0300

    delete command.

---
+++ CleanArchitecture.Application/Features/Todos/DeleteTodo/DeleteTodoByIdCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿
+
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
+using MediatR;
+
+namespace CleanArchitecture.Application.Features.Todos.DeleteTodo;
+
+internal sealed class DeleteTodoByIdCommandHandler(ITodoRepository todoRepository) : IRequestHandler<DeleteTodoByIdComamnd>
+{
+    public async Task Handle(DeleteTodoByIdComamnd request, CancellationToken cancellationToken)
+    {
+        Todo? todo = await todoRepository.GetByIdAsync(request.id, cancellationToken);
+        if(todo is null)
+        {
+            throw new ArgumentNullException("todo yok");
+        }
+        await todoRepository.DeleteAsync(todo, cancellationToken);
+    }
+}
---
+++ CleanArchitecture.Application/Features/Todos/DeleteTodo/DeleteTodoCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Features.Todos.DeleteTodo;
+
+public sealed record DeleteTodoByIdComamnd(Guid id): IRequest;
