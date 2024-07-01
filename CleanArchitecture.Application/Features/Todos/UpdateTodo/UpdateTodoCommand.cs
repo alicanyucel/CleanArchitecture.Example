ce5bd25060ca2642e74a3e420a9860b282ac7213
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-01 21:30:26 +0300

    ...

---
+++ CleanArchitecture.Application/Features/Todos/UpdateTodo/UpdateTodoCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Features.Todos.UpdateTodo;
+
+public sealed record UpdateTodoCommand(Guid Id, string Work, DateOnly DeadLine, string? Note, bool IsCompleted) : IRequest;
---
+++ CleanArchitecture.Application/Features/Todos/UpdateTodo/UpdateTodoCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Repositories;
+using MediatR;
+
+namespace CleanArchitecture.Application.Features.Todos.UpdateTodo;
+
+internal sealed class UpdateTodoCommandHandler(ITodoRepository todoRepository) : IRequestHandler<UpdateTodoCommand>
+{
+    public async Task Handle(UpdateTodoCommand request, CancellationToken cancellationToken)
+    {
+        Todo? todo = await todoRepository.GetByIdAsync(request.Id, cancellationToken);
+        if (todo is null)
+        {
+            throw new ArgumentNullException("todo kaydı yok");
+
+        }
+        todo.Work = request.Work;
+        todo.DeadLine = request.DeadLine;
+        todo.Note = request.Note;
+        todo.IsCompleted = request.IsCompleted;
+        await todoRepository.UpdateAsync(todo, cancellationToken);
+    }
+}
